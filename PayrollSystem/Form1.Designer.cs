f3664e7bc53b3760994e8f5ca838eafe7fd6ecf2
ghouse <mgmohiuddin2001@gmail.com>
2022-08-22 07:18:54 +0530

    Added files.

---
+++ PayrollSystem/Form1.Designer.cs
@@ -0,0 +1,219 @@
+﻿namespace PayrollSytem
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.tsEmployee = new System.Windows.Forms.ToolStripButton();
+            this.tsPayroll = new System.Windows.Forms.ToolStripButton();
+            this.tsUser = new System.Windows.Forms.ToolStripButton();
+            this.tsReport = new System.Windows.Forms.ToolStripButton();
+            this.tsLogin = new System.Windows.Forms.ToolStripButton();
+            this.AdminName = new System.Windows.Forms.ToolStripLabel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.toolStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.toolStrip1.ImageScalingSize = new System.Drawing.Size(50, 34);
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.tsEmployee,
+            this.tsPayroll,
+            this.tsUser,
+            this.tsReport,
+            this.tsLogin,
+            this.AdminName});
+            this.toolStrip1.Location = new System.Drawing.Point(0, 0);
+            this.toolStrip1.Margin = new System.Windows.Forms.Padding(13, 12, 13, 12);
+            this.toolStrip1.MaximumSize = new System.Drawing.Size(133, 0);
+            this.toolStrip1.MinimumSize = new System.Drawing.Size(160, 0);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(160, 622);
+            this.toolStrip1.TabIndex = 0;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // tsEmployee
+            // 
+            this.tsEmployee.AutoSize = false;
+            this.tsEmployee.Enabled = false;
+            this.tsEmployee.Image = ((System.Drawing.Image)(resources.GetObject("tsEmployee.Image")));
+            this.tsEmployee.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.tsEmployee.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsEmployee.Name = "tsEmployee";
+            this.tsEmployee.Size = new System.Drawing.Size(110, 80);
+            this.tsEmployee.Text = "Employees";
+            this.tsEmployee.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.tsEmployee.Click += new System.EventHandler(this.tsEmployee_Click);
+            // 
+            // tsPayroll
+            // 
+            this.tsPayroll.AutoSize = false;
+            this.tsPayroll.Enabled = false;
+            this.tsPayroll.Image = ((System.Drawing.Image)(resources.GetObject("tsPayroll.Image")));
+            this.tsPayroll.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.tsPayroll.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsPayroll.Name = "tsPayroll";
+            this.tsPayroll.Size = new System.Drawing.Size(110, 80);
+            this.tsPayroll.Text = "Payroll";
+            this.tsPayroll.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.tsPayroll.Click += new System.EventHandler(this.tsPayroll_Click);
+            // 
+            // tsUser
+            // 
+            this.tsUser.AutoSize = false;
+            this.tsUser.Enabled = false;
+            this.tsUser.Image = ((System.Drawing.Image)(resources.GetObject("tsUser.Image")));
+            this.tsUser.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.tsUser.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsUser.Name = "tsUser";
+            this.tsUser.Size = new System.Drawing.Size(110, 80);
+            this.tsUser.Text = "Users";
+            this.tsUser.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.tsUser.Click += new System.EventHandler(this.tsUser_Click);
+            // 
+            // tsReport
+            // 
+            this.tsReport.AutoSize = false;
+            this.tsReport.Enabled = false;
+            this.tsReport.Image = ((System.Drawing.Image)(resources.GetObject("tsReport.Image")));
+            this.tsReport.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.tsReport.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsReport.Name = "tsReport";
+            this.tsReport.Size = new System.Drawing.Size(110, 80);
+            this.tsReport.Text = "Reports";
+            this.tsReport.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.tsReport.Click += new System.EventHandler(this.tsReport_Click);
+            // 
+            // tsLogin
+            // 
+            this.tsLogin.AutoSize = false;
+            this.tsLogin.Image = ((System.Drawing.Image)(resources.GetObject("tsLogin.Image")));
+            this.tsLogin.ImageAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.tsLogin.ImageTransparentColor = System.Drawing.Color.Magenta;
+            this.tsLogin.Name = "tsLogin";
+            this.tsLogin.Size = new System.Drawing.Size(110, 80);
+            this.tsLogin.Text = "Login";
+            this.tsLogin.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.tsLogin.Click += new System.EventHandler(this.tsLogin_Click);
+            // 
+            // AdminName
+            // 
+            this.AdminName.Alignment = System.Windows.Forms.ToolStripItemAlignment.Right;
+            this.AdminName.AutoSize = false;
+            this.AdminName.Name = "AdminName";
+            this.AdminName.Size = new System.Drawing.Size(108, 15);
+            this.AdminName.Text = "Admin";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(982, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 32);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Done by:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(1140, 20);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(155, 32);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Abid Khan";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(1140, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(120, 32);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Ghouse";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(1140, 119);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(120, 32);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Ganesh";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1324, 622);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.toolStrip1);
+            this.Margin = new System.Windows.Forms.Padding(4);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Payroll System - Dashboard";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.ToolStripButton tsEmployee;
+        private System.Windows.Forms.ToolStripButton tsPayroll;
+        private System.Windows.Forms.ToolStripButton tsUser;
+        private System.Windows.Forms.ToolStripButton tsReport;
+        private System.Windows.Forms.ToolStripButton tsLogin;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.ToolStripLabel AdminName;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+    }
+}
+
---
+++ PayrollSystem/frm_login.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace PayrollSytem
+{
+    partial class frm_login
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_login = new System.Windows.Forms.Button();
+            this.txt_username = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txt_password = new System.Windows.Forms.TextBox();
+            this.btn_cancel = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label3 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btn_login
+            // 
+            this.btn_login.Location = new System.Drawing.Point(52, 328);
+            this.btn_login.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_login.Name = "btn_login";
+            this.btn_login.Size = new System.Drawing.Size(199, 30);
+            this.btn_login.TabIndex = 1;
+            this.btn_login.Text = "Login";
+            this.btn_login.UseVisualStyleBackColor = true;
+            this.btn_login.Click += new System.EventHandler(this.btn_login_Click);
+            // 
+            // txt_username
+            // 
+            this.txt_username.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txt_username.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_username.Location = new System.Drawing.Point(52, 136);
+            this.txt_username.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_username.Name = "txt_username";
+            this.txt_username.Size = new System.Drawing.Size(413, 30);
+            this.txt_username.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label1.Location = new System.Drawing.Point(47, 95);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(129, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Username : ";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(47, 195);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 25);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Password :";
+            // 
+            // txt_password
+            // 
+            this.txt_password.BackColor = System.Drawing.SystemColors.ScrollBar;
+            this.txt_password.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txt_password.Location = new System.Drawing.Point(52, 237);
+            this.txt_password.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txt_password.Name = "txt_password";
+            this.txt_password.Size = new System.Drawing.Size(413, 30);
+            this.txt_password.TabIndex = 2;
+            this.txt_password.UseSystemPasswordChar = true;
+            // 
+            // btn_cancel
+            // 
+            this.btn_cancel.Location = new System.Drawing.Point(278, 328);
+            this.btn_cancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btn_cancel.Name = "btn_cancel";
+            this.btn_cancel.Size = new System.Drawing.Size(187, 30);
+            this.btn_cancel.TabIndex = 5;
+            this.btn_cancel.Text = "Cancel";
+            this.btn_cancel.UseVisualStyleBackColor = true;
+            this.btn_cancel.Click += new System.EventHandler(this.btn_cancel_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BackColor = System.Drawing.Color.PaleTurquoise;
+            this.pictureBox1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(512, 410);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BackColor = System.Drawing.Color.Transparent;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.label3.Location = new System.Drawing.Point(117, 29);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(267, 25);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Income Monitoring System";
+            // 
+            // frm_login
+            // 
+            this.AcceptButton = this.btn_login;
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(512, 410);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.btn_cancel);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txt_password);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txt_username);
+            this.Controls.Add(this.btn_login);
+            this.Controls.Add(this.pictureBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.SizableToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MinimizeBox = false;
+            this.Name = "frm_login";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Login";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_login;
+        private System.Windows.Forms.TextBox txt_username;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txt_password;
+        private System.Windows.Forms.Button btn_cancel;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label3;
+    }
+}
